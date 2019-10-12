b7a552345b05ee42885f5f8c1f29e2e3aeef08ed
geepawhill <geepawhill@geepawhill.org>
2019-10-12 13:47:12 -0400

    Repackaging to prod.

---
+++ core/src/main/kotlin/org/geepawhill/tsd/core/TsdOutput.kt
@@ -0,0 +1,57 @@
+package org.geepawhill.tsd.core
+
+import java.util.*
+
+class TsdOutput : TsdWriter {
+    val prefixes = Stack<String>()
+    val keyToLeaf = mutableMapOf<String, String>()
+
+    override operator fun set(key: String, value: String) {
+        checkSetKey(key)
+        keyToLeaf[addPrefix(key)] = value
+    }
+
+    override operator fun <T> set(key: String, value: T) {
+        set(key, value.toString())
+    }
+
+    override operator fun set(key: String, value: Tsd) {
+        checkSetKey(key)
+        prefixes.push(key)
+        value.tsdPut(this)
+        prefixes.pop()
+    }
+
+    override operator fun <T> set(key: String, collection: Collection<T>) {
+        var index = 0
+        collection.forEach {
+            set("$key[${index++}]", it)
+        }
+    }
+
+    override operator fun set(key: String, op: () -> Unit) {
+        checkSetKey(key)
+        prefixes.push(key)
+        op()
+        prefixes.pop()
+    }
+
+    operator fun get(key: String): String {
+        return keyToLeaf.getOrElse(key) { throw TsdWriter.UnknownKeyException(key) }
+    }
+
+    fun dump() {
+        for (entry in keyToLeaf.entries) {
+            println("${entry.key}=${entry.value}")
+        }
+    }
+
+    private fun checkSetKey(key: String) {
+        if (key.contains(".")) throw TsdWriter.IllegalKeyException(key, "contains period")
+        if (key.isBlank()) throw TsdWriter.IllegalKeyException(key, "is blank")
+        if (keyToLeaf[key] != null) throw TsdWriter.IllegalKeyException(key, "is already assigned")
+    }
+
+    private fun addPrefix(key: String) = if (!prefixes.empty()) prefixes.joinToString(".") + "." + key else key
+
+}
